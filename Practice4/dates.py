0acc5619c99f9eed65cc1071464c242fac5eb7e5
zfarabin-cloud <zfarabin@gmail.com>
2026-02-17 12:31:09 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

---
+++ Practice4/dates.py
@@ -0,0 +1,7 @@
+# dates
+'''
+Write a Python program to subtract five days from current date.
+Write a Python program to print yesterday, today, tomorrow.
+Write a Python program to drop microseconds from datetime.
+Write a Python program to calculate two date difference in seconds.
+'''
---
+++ Practice4/generators.py
@@ -0,0 +1,9 @@
+# generators
+'''
+Create a generator that generates the squares of numbers up to some number N.
+Write a program using generator to print the even numbers between 0 and n in comma separated form where n is input from console.
+Define a function with a generator which can iterate the numbers, which are divisible by 3 and 4, between a given range 0 and n.
+Implement a generator called squares to yield the square of all numbers from (a) to (b). Test it with a "for" loop and print each of the yielded values.
+Implement a generator that returns all numbers from (n) down to 0.
+'''
+
